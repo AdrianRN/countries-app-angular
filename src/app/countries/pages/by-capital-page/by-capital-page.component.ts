8f7bedb011e9834068cd1504725cc4d517c0e829
AdrianRN <ary706@hotmail.com>
2023-11-03 22:09:20 -0600

    ByCapital Component

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country.interface';
 
@@ -8,18 +8,29 @@ import { Country } from '../../interfaces/country.interface';
   styles: [
   ]
 })
-export class ByCapitalPageComponent {
+export class ByCapitalPageComponent implements OnInit {
 
   public countries:Country[] = [];
+  public initialValue: string = '';
+  public isLoading: boolean = false;
 
   constructor(private countriesService: CountriesService ) {}
+
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCapital.countries;
+    this.initialValue = this.countriesService.cacheStore.byCapital.term;
+  }
 
 
 
   searchByCapital(term: string) {
+
+    this.isLoading = true;
+
     this.countriesService.searchCapital(term)
       .subscribe( countries => {
         this.countries = countries;
+        this.isLoading = false;
       });
 
   }
