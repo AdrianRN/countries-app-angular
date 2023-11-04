9c73dc8e75c85af55af68390920f58871084873c
AdrianRN <ary706@hotmail.com>
2023-11-03 22:10:33 -0600

    byCountry Component

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country.interface';
 
@@ -8,17 +8,28 @@ import { Country } from '../../interfaces/country.interface';
   styles: [
   ]
 })
-export class ByCountryPageComponent {
+export class ByCountryPageComponent implements OnInit {
 
   public countries:Country[] = [];
+  public initialValue: string = '';
+  public isLoading: boolean = false;
 
   constructor(private countriesService: CountriesService) {}
 
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCountry.countries
+    this.initialValue = this.countriesService.cacheStore.byCountry.term;
+  }
+
 
   searchByCountry(term: string) {
+
+    this.isLoading = true;
+
     this.countriesService.searchCountry(term)
       .subscribe( countries => {
         this.countries = countries;
+        this.isLoading = false;
       });
 
   }
